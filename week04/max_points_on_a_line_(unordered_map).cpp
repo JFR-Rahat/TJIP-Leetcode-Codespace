da89fd292a0a443c2caa1a8ddfbcc324f897184c
JFR-Rahat <100526578+JFR-Rahat@users.noreply.github.com>
2023-08-10 02:56:53 +0600

    Max Points on a Line with unordered map

---
+++ week04/max_points_on_a_line_(unordered_map).cpp
@@ -0,0 +1,39 @@
+// All the points having the same slope value
+// lies on the same line. So for each points,
+// counting every possible slope and then
+// we can figure out the maximum number of
+// points through this. 
+
+// TC = O(n^2)
+// SC = O(n)
+
+class Solution {
+public:
+    int maxPoints(vector<vector<int>>& points) {
+        
+        unordered_map<long long, int> slope_points;
+        int maxPoints = 0;
+
+        for(int i=0; i<points.size(); i++){
+            slope_points.clear();
+
+            for(int j=i+1; j<points.size(); j++){
+
+                int dy = points[j][1] - points[i][1];
+                int dx = points[j][0] - points[i][0];
+
+                int gcd = __gcd(dy, dx);
+                dy /= gcd;
+                dx /= gcd;
+
+                slope_points[(100000LL * dx) + dy]++;
+            }
+
+            for(auto &slope: slope_points){
+                maxPoints = max(maxPoints, slope.second);
+            }
+        }
+
+        return maxPoints + 1;
+    }
+};
